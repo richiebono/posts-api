02a48ee3c8d5656dcf9ce23e2628c058329aa7b3
richiebono <richiebono@gmail.com>
2022-10-27 11:12:11 +0200

    Create redis rate limit health check.

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { HealthCheck, HealthCheckService, HttpHealthIndicator, MongooseHealthIndicator } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator, MicroserviceHealthIndicator, MongooseHealthIndicator } from '@nestjs/terminus';
+import { RedisOptions, Transport } from '@nestjs/microservices';
 
 @ApiTags('health')
 @Controller('health')
@@ -10,6 +11,7 @@ export class HealthController {
     private healthCheckService: HealthCheckService,
     private http: HttpHealthIndicator,
     private db: MongooseHealthIndicator,
+    private microservice: MicroserviceHealthIndicator
   ){}
 
   @Get()
@@ -18,8 +20,15 @@ export class HealthController {
     return this.healthCheckService.check([
         () => this.http.pingCheck('External Posts URI Check',  process.env.JSON_PLACE_HOLDER_POST_API),
         () => this.http.pingCheck('External User URI Check', process.env.JSON_PLACE_HOLDER_USER_API),
-        () => this.http.pingCheck('API Check', process.env.HEALTH_CHECK_URI),
-        () => this.db.pingCheck('mongodb', { timeout: parseInt(process.env.HEALTH_CHECK_DB_TIMEOUT) })
+        () => this.http.pingCheck('Posts API Check', process.env.HEALTH_CHECK_URI),
+        () => this.db.pingCheck('mongodb', { timeout: parseInt(process.env.HEALTH_CHECK_DB_TIMEOUT) }),
+        () => this.microservice.pingCheck<RedisOptions>('redis-rate-limit', {
+          transport: Transport.REDIS,
+          options: {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT)
+         }
+        }),
     ]);
   }  
 }
